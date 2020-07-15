a6790be9ff5dfb856f2c87cf3bd2b96323c7adc8
Denisse Gruyere <dgruyere@oa-e5.msk.21-school.ru>
2020-07-15 23:35:11 +0300

    some first tests passed

--- main.c
+++ main.c
@@ -13,16 +13,16 @@ int main(void)
 	nickname2 = "dgruyere";
 
 
-	 ft_printf("hello my nickname is %s %s %s\n", nickname1, nickname2, nickname1);
+	/*ft_printf("hello my nickname is %s %s %s\n", nickname1, nickname2, nickname1);
 	ft_printf("hello! The bomb is ticking down, %s: %d, %d, %d\n", nickname2, 3, 2, 1);
 	ft_printf("%s, %d in octal form is %o, in hexadecimal %x\n", nickname1, 100, 100, 100);
-	ft_printf("hello my another nickname is %-20s .\n", "10");
+	ft_printf("hello my another nickname is %-20s .\n", "10");*/
 
 	//simple TESTS FROM https://alvinalexander.com/programming/printf-format-cheat-sheet/
 	//integer formatting
 
 	//ft_printf("hello%#o\n", 10);
-	ft_printf("hello %#lx\n", "hello");
+	/*ft_printf("hello %#lx\n", "hello");
 	printf("hello %p\n", "hello");
 	printf("hello %#lx\n", "hello");
 	printf("hello %#o\n", 10);
@@ -63,8 +63,59 @@ int main(void)
 	ft_printf("%-10s\n", "Hello");						//'Hello     '
 	ft_printf("%s\n", nickname1);
 	ft_printf("%10s\n", nickname2);
-	ft_printf("%-10s\n", nickname1);
+	ft_printf("%-10s\n", nickname1);*/
 	//TODO check hh h ll l with printf tests
+
+	//ft_printf("%%");
+	ft_putchar('\n');
+	printf("%%");
+	ft_putchar('\n');
+	ft_putchar('\n');
+	ft_printf("%5%");
+	ft_putchar('\n');
+	printf("%5%");
+	ft_putchar('\n');
+	ft_putchar('\n');
+	ft_printf("%-5%");
+	ft_putchar('\n');
+	printf("%-5%");
+	ft_putchar('\n');
+	ft_putchar('\n');
+	ft_printf("%05");
+	ft_putchar('\n');
+	printf("%05%");
+	ft_putchar('\n');
+	ft_putchar('\n');
+	ft_printf("%-05s");
+	ft_putchar('\n');
+	printf("%-05s%");
+	/*
+	Test  12 (nocrash_nullspec_5w) : FAILED.
+			(nocrash test: automatically passes provided no unexpected failure)
+	First line of code: {return test("%5");}
+	SEGFAULT
+
+	Test  13 (nocrash_nullspec_5wzp) : FAILED.
+			(nocrash test: automatically passes provided no unexpected failure)
+	First line of code: {return test("%05");}
+	SEGFAULT
+
+	Test  14 (nocrash_nullspec_5wzplj) : FAILED.
+			(nocrash test: automatically passes provided no unexpected failure)
+	First line of code: {return test("%-05");}
+	SEGFAULT
+
+	Test  15 (nocrash_nullspec_5wlj) : FAILED.
+			(nocrash test: automatically passes provided no unexpected failure)
+	First line of code: {return test("%-5");}
+	SEGFAULT
+
+	Test  77 (s_prec_03_no_width_s_hidden) : FAILED.
+			First line of code: {return test("%.03s", s_hidden);}
+	expected output : "hi "
+	your output     : "3s"
+	expected (nonprintable as hex) : "hi "
+	actual   (nonprintable as hex) : "3s"*/
 
 	return 0;
 }
